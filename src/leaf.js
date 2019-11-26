4ddf651ac46a0269c98ebe0635686df414c86e9a
Bob den Otter <bob@twokings.nl>
2019-11-26 21:06:53 +0100

    Working

--- src/leaf.js
+++ src/leaf.js
@@ -1,4 +1,14 @@
 
 console.log('The answer, my friend, is blowin’ in the wind. 🍃');
 
-console.log('joe');
+// Make Bulma dropdown work
+var dropdown = document.querySelector('.dropdown');
+
+dropdown.addEventListener('click', function(event) {
+
+    // If the clicked element doesn't have the right selector, bail
+    if (event.target.matches('.input')) {
+        return;
+    }
+    dropdown.classList.toggle('is-active');
+}, false);
